77582aef3982df62dd8c4a30afd387b8962f5f44
David Walczak <dwalczak@usf.edu>
2025-09-30 13:49:08 -0400

    added the company class and console log for it, and the call function

--- cc_6.js
+++ cc_6.js
@@ -7,13 +7,13 @@ class Employees{
 
 
     describe(){
-        return(`${this.name} works in this department: ${this.department} and is a ${this.level}`)
+        return(`${this.name} works in the ${this.department} department.`)
     };
 };
 
 class Manager extends Employees{
     constructor(name, department, teamSize) {
-        super(name, department, level);
+        super(name, department);
         this.teamSize = teamSize;
     }
 
@@ -28,7 +28,36 @@ let emp2 = new Employees("Jebediah", "Butter Making")
 let emp3 = new Employees("Billy", "Family Business")
 let emp4 = new Employees("Joel", "Scout Master")
 
+let mang1 = new Manager("Bosephus", "Sulfur Production", 30)
+let mang2 = new Manager("Alexander", "Accounting", 5)
 
+// Company class
+class Company{
+    constructor() {
+        this.employees = [];
+    }
+
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+
+    listEmployees(){
+        this.employees.forEach(emp => {
+            console.log(emp.describe());
+        });
+    }
+};
+
+// Step 6: Instantiate Company
+const myCompany = new Company();
+myCompany.addEmployee(emp1);
+myCompany.addEmployee(emp2);
+myCompany.addEmployee(emp3);
+myCompany.addEmployee(emp4);
+myCompany.addEmployee(mang1);
+myCompany.addEmployee(mang2);
+
+myCompany.listEmployees();
 
 function promoteLevel(level){    
     if (level === "Employee"){
